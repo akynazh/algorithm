8ae797263071b1786bf8bb5ca0a509afb021292f
akynazh <1945561232@qq.com>
2022-10-14 17:05:50 +0800

    update

--- leetcode/offer/create.py
+++ leetcode/offer/create.py
@@ -1,18 +1,17 @@
 import os
 i = int(input())
-path = "D:/algorithm/LeetCode/offer/" + str(i)
+path = f'{os.getenv("CODE")}/algorithm/leetcode/offer/{i}'
 os.mkdir(path)
-f = open(path + "/" + str(i) + ".cpp", "w")
-f.write("#include <bits/stdc++.h>\n")
-f.write("using namespace std;\n")
-f.write("class Solution {\n")
-f.write("\n")
-f.write("};\n")
-f.write("int main() {\n")
-f.write("    \n")
-f.write("    Solution solution;\n")
-f.write("    \n")
-f.write("    return 0;\n")
-f.write("}\n")
-f.close()
-i += 1;
+
+with open(path + "/" + str(i) + ".cpp", "w") as f:
+    f.write("#include <bits/stdc++.h>\n")
+    f.write("using namespace std;\n\n")
+    f.write("class Solution {\n")
+    f.write("\n")
+    f.write("};\n\n")
+    f.write("int main() {\n")
+    f.write("    \n")
+    f.write("    Solution solution;\n")
+    f.write("    \n")
+    f.write("    return 0;\n")
+    f.write("}\n")
