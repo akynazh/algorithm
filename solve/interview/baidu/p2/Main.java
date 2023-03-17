95b94f6e1966c20d19bbe42dbac8d76925b6143f
akynazh <akynazh@qq.com>
2023-03-17 12:02:37 +0800

    update

---
+++ solve/interview/baidu/p2/Main.java
@@ -0,0 +1,5 @@
+package interview.baidu.p2;
+
+// 计算回文子串
+public class Main {
+}
---
+++ test/cpp/test_memset.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[100];
+    memset(&arr, -1, sizeof(arr));
+    for (int i = 0; i < 100; i++) cout << arr[i];
+    return 0;
+}
