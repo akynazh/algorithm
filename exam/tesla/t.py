e23d802be7741216b52868e761a13c0e04eee70d
Zhihang Jiang <zhihang.jiang@shopee.com>
2024-12-05 16:18:19 +0800

    update

---
+++ exam/tesla/t.py
@@ -0,0 +1,21 @@
+# tesla
+# vis
+
+ans = 0
+mp = {}
+
+
+def dfs(lst, vis, res):
+    if len(res) == len(lst):
+        print(res)
+        return
+    for i in range(len(lst)):
+        if vis[i] == 0:
+            vis[i] = 1
+            dfs(lst, vis, res + lst[i])
+            vis[i] = 0
+
+
+def solve(lst):
+    vis = [0 for _ in range(lst)]
+    dfs(lst, vis, "")
